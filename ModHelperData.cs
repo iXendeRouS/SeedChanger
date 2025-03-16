041bf0e3c4e197f04a6ac56b24d8bc1d8b3fc705
iXendeRouS <aharrison9608@gmail.com>
2025-03-16 18:36:16 +1300

    updated description

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "2.1.0";
     public const string Name = "SeedChanger";
 
-    public const string Description = "Change and view the seed used to randomly generate freeplay rounds.";
+    public const string Description = "Change and view the seed used to randomly generate freeplay rounds. Can only change the seed when not in freeplay already.";
 
     public const string RepoOwner = "iXendeRouS";
     public const string RepoName = "SeedChanger";
