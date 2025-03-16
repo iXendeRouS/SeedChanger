4c22571554565c361c7b8637b4fc04f97aad9a4d
iXendeRouS <aharrison9608@gmail.com>
2025-03-16 18:35:09 +1300

    updated version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace SeedChanger;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "47.0";
-    public const string Version = "2.0.0";
+    public const string Version = "2.1.0";
     public const string Name = "SeedChanger";
 
     public const string Description = "Change and view the seed used to randomly generate freeplay rounds.";
